d0bd7069bd045bf49252efcf4a7fd653ddc08b6f
Diego <diegopablo012@gmail.com>
2024-03-26 18:15:39 -0600

    feat: backend

---
+++ backend/app.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send('Bienvenido a MK Platform Backend');
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
