c7feec1d5087dc6692edf09e59c2cffd414c8f2a
Diego <diegopablo012@gmail.com>
2024-03-31 21:39:11 -0600

    feat: card

--- client-frontend/tailwind.config.js
+++ client-frontend/tailwind.config.js
@@ -5,7 +5,23 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'color-text': '#030303',
+        'color-symbols': '#262932',
+        'calendar-days': '#716A6E',
+        'calendar-selectday': '#6FB74B',
+        'color-tittlesAdmin': '#3B6B27',
+        'color-prices':'#426B1F',
+        'top-banners': '#B8B3B6',
+        'color-componentes': '#FAFAF5',
+        'color-backgroundText': '#FFFFFF',
+        'color-searcher': '#D9D9D9',
+        'color-contorno': '#E6E6E6',
+        'color-contornoButtons': '#000000',
+        'color-shadow': '#7a7a7a',
+      },
+    },
   },
   plugins: [],
 }
