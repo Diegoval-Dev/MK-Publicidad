a4049ffed58e09a1ecfbdd55b14c3e2c323e1046
Diego <diegopablo012@gmail.com>
2024-04-02 10:56:10 -0600

    feat: start filter

---
+++ client-frontend/src/components/Filter.jsx
@@ -0,0 +1,14 @@
+/*
+@Ruth
+@Silvia
+*/
+function Filtro(){
+    return (
+        <div>
+            <input type="text" placeholder="Buscar..." />
+            <button>Buscar</button>
+        </div>
+    )
+}
+
+export default Filtro;
