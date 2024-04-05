b83dea67be92f36f16760e0d244c3e469aea8a9e
Anaru03 <dele22428@uvg.edu.gt>
2024-04-05 17:23:33 -0600

    Component Styling pt.2

--- client-frontend/src/components/BannerSearch.jsx
+++ client-frontend/src/components/BannerSearch.jsx
@@ -4,7 +4,7 @@ function BannerSearch() {
         <div>
       <form>
         <input type="text" placeholder="Buscar..." className="pl-4 border border-black-500 pe-20 py-2 rounded-lg focus:outline-none focus:border-green-500 mx-1" />
-        <input type="submit" value="Buscar" className="border border-gray-300 px-4 py-2 rounded-lg ml-2 bg-800 text-white hover:bg-green-600 cursor-pointer" />
+        <input type="submit" value="Buscar" className="border border-gray-300 px-4 py-2 rounded-lg ml-2 bg-800 text-white hover:bg-green-600 cursor-pointer bg-lime-500" />
       </form>
     </div>
     );
