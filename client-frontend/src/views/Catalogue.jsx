09cdad07db16f36b2a725517fe389e8cc6ab7f95
Anaru03 <dele22428@uvg.edu.gt>
2024-04-16 11:19:42 -0600

    Individual component for navigation buttons and filters.

--- client-frontend/src/views/Catalogue.jsx
+++ client-frontend/src/views/Catalogue.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import Banner from '../components/Banner';
 import ProductList from '../components/ProductList';
+import NavigationButtons from '../components/NavigationButtons';
 import Filter from '../components/Filter';
 import '../styles/styles.css'
 
@@ -39,34 +40,32 @@ function Catalogue({ setScreen }) {
     setIsFilterVisible(!isFilterVisible);
   };
 
-  
-    return (
-      <div style={{display: "flex", alignItems: "center", flexDirection: "column"}}>
-        <Banner />
-        <div className='container flex space-x-4 justify-between'>
-        <button onClick={goToHomePage} className="py-2 px-4 bg-lime-500 text-black rounded">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-          <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
-          </svg>
-        </button>
-        <button onClick={toggleFilterVisibility} className="py-2 px-4 bg-lime-500 text-black rounded">
-          {isFilterVisible ? "" : <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 6h9.75M10.5 6a1.5 1.5 0 1 1-3 0m3 0a1.5 1.5 0 1 0-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-9.75 0h9.75" />
-            </svg>
-            }
-        </button>
-        </div>
-        {isFilterVisible && (
-          <Filter 
-            toggleFilterVisibility={toggleFilterVisibility} 
-            tempFilters={tempFilters}
-            setTempFilters={setTempFilters} 
-            handleApplyFilters={handleApplyFilters}
-            handleClearFilters={handleClearFilters}
-          />)}
-        <ProductList category={"A"} material={appliedFilters.material} technique={appliedFilters.technique} size={appliedFilters.size} color={appliedFilters.color} />
-      </div>
-    )
+  return (
+    <div style={{ display: "flex", alignItems: "center", flexDirection: "column" }}>
+      <Banner />
+      <NavigationButtons
+        goToHomePage={goToHomePage}
+        toggleFilterVisibility={toggleFilterVisibility}
+        isFilterVisible={isFilterVisible}
+      />
+      {isFilterVisible && (
+        <Filter
+          toggleFilterVisibility={toggleFilterVisibility}
+          tempFilters={tempFilters}
+          setTempFilters={setTempFilters}
+          handleApplyFilters={handleApplyFilters}
+          handleClearFilters={handleClearFilters}
+        />
+      )}
+      <ProductList
+        category={"A"}
+        material={appliedFilters.material}
+        technique={appliedFilters.technique}
+        size={appliedFilters.size}
+        color={appliedFilters.color}
+      />
+    </div>
+  )
 }
 
 export default Catalogue;
