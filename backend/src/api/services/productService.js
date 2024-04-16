19a3cf6f534a9fbf83fb33cadd61aefafe1efcf3
michellemej22596 <mej22596@uvg.edu.gt>
2024-04-16 11:57:53 -0600

    Feat: Endpoints unified

--- backend/src/api/services/productService.js
+++ backend/src/api/services/productService.js
@@ -2,109 +2,63 @@ import Product from "../../models/productModel.js";
 import { Op } from "sequelize";
 
 const createProduct = async (product) => {
-    console.log("Hola desde servicio",product);
     try {
-        const newProduct = await Product.create(product);
-        return newProduct;
+        return await Product.create(product);
     } catch (error) {
         throw error;
     }
-}
+};
 
-const getProducts = async () => {
+const getProducts = async (filters = {}) => {
     try {
-        const products = await Product.findAll();
-        return products;
+        const whereClause = {};
+        if (filters.name) {
+            whereClause.name = { [Op.like]: `%${filters.name}%` };
+        }
+        if (filters.material) {
+            whereClause.material = filters.material;
+        }
+        if (filters.category) {
+            whereClause.category = filters.category;
+        }
+
+        return await Product.findAll({ where: whereClause });
     } catch (error) {
         throw error;
     }
-}
+};
 
 const getProductById = async (id) => {
     try {
-        const product = await Product.findByPk(id);
-        return product;
+        return await Product.findByPk(id);
     } catch (error) {
         throw error;
     }
-}
+};
 
 const updateProduct = async (id, product) => {
     try {
-        const productToUpdate = await Product.update(product, {
-            where: {
-                id: id
-            }
-        });
-        const updatedProduct = await Product.findByPk(id);
-        return updatedProduct;
+        await Product.update(product, { where: { id } });
+        return await Product.findByPk(id);
     } catch (error) {
         throw error;
     }
-}
+};
 
 const deleteProduct = async (id) => {
     try {
         const product = await Product.findByPk(id);
-        await Product.destroy({
-            where: {
-                id: id
-            }
-        });
+        await product.destroy();
         return product;
     } catch (error) {
         throw error;
     }
-}
-
-const getProductsByName = async (name) => {
-    try {
-        const products = await Product.findAll({
-            where: {
-                name: {
-                    [Op.like]: `%${name}%`
-                }
-            }
-        });
-        return products;
-    } catch (error) {
-        throw error;
-    }
-}
-
-const getProductsByCategory = async (category) => {
-    try {
-        const products = await Product.findAll({
-            where: {
-                category: category 
-            }
-        });
-        return products;
-    } catch (error) {
-        throw error;
-    }
-}
-
-const getProductsByMaterial = async (material) => {
-    try {
-        const products = await Product.findAll({
-            where: {
-                material: material
-            }
-        });
-        return products;
-    } catch (error) {
-        throw error;
-    }
-}
+};
 
 export default {
     createProduct,
     getProducts,
     getProductById,
     updateProduct,
-    deleteProduct,
-    getProductsByName,
-    getProductsByCategory,
-    getProductsByMaterial,
+    deleteProduct
 };
