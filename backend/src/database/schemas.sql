982aa7aac0fdfb43dae2c633e244c5d179deca95
michellemej22596 <mej22596@uvg.edu.gt>
2024-05-04 18:28:44 -0600

    SCRUM 28 - Quotes tables added

--- backend/src/database/schemas.sql
+++ backend/src/database/schemas.sql
@@ -20,6 +20,23 @@ user_phone VARCHAR(50) not null,
 user_officePhone VARCHAR(50) not null
 );
 
+CREATE TABLE quotes (
+    quotation_id INT PRIMARY KEY AUTO_INCREMENT,
+    customer_nit INT NOT NULL,
+    quote_no VARCHAR(50) NOT NULL,
+    quote_date DATE NOT NULL,
+    quote_sellerId INT NOT NULL,
+    quote_validityTill DATE NOT NULL,
+    quote_shippingTime VARCHAR(50) NOT NULL,
+    quote_payMethod VARCHAR(50) NOT NULL,
+    quote_credit BOOLEAN NOT NULL,
+    quote_payForm VARCHAR(50) NOT NULL,
+    productId INT NOT NULL,
+    quote_status VARCHAR(50) NOT NULL,
+    FOREIGN KEY (productId) REFERENCES products(id)
+);
+
+
 ALTER TABLE users
 ADD COLUMN createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
 ADD COLUMN updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP;
