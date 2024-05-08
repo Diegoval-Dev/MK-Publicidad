34eba3a98c19680b22d1407987ab8642b26b5ba6
Silviaillescas <silviaillesca402@gmail.com>
2024-05-07 19:54:23 -0600

    Add Customer model definition

---
+++ backend/src/models/customersModel.js
@@ -0,0 +1,34 @@
+import { DataTypes } from 'sequelize';
+import { db } from '../database/config.js';
+
+const Customer = db.define('Customer', {
+    Customer_ID: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        unique: true,
+        allowNull: false
+    },
+    customer_company: {
+        type: DataTypes.STRING(50),
+        allowNull: false
+    },
+    customer_email: {
+        type: DataTypes.STRING(50),
+        allowNull: false
+    },
+    customer_contact: {
+        type: DataTypes.STRING(15),
+        allowNull: false
+    },
+    customer_address: {
+        type: DataTypes.STRING(255),
+        allowNull: false
+    }
+}, {
+    tableName: 'Customers', 
+    paranoid: true, 
+    timestamps: false 
+});
+
+export default Customer;
