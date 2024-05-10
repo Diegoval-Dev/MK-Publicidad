191168045ea2aac6fc281dd56dd57d3f688e5616
Silviaillescas <silviaillesca402@gmail.com>
2024-05-09 22:33:36 -0600

    Display product categories and images in ProducHomeList

--- client-frontend/src/components/ProducHomeList.jsx
+++ client-frontend/src/components/ProducHomeList.jsx
@@ -1,34 +1,36 @@
 import PropTypes from 'prop-types';
-import ProductCategory from "./ProductCategory";
+import ProductCategory from './ProductCategory';
 import useNavigate from '@hooks/useNavigate';
 
-function ProductHomeList({products}) {
-  const { navigate, params } = useNavigate();
-
-  const categories = Array.from(new Set(products.map(product => product.category)));
-
+function ProducHomeList({ products = [], categories = [] }) {
+  const { navigate } = useNavigate();
 
   return (
     <div className="container mx-auto border border-black-300 p-4 mt-5 shadow-md rounded-lg">
-      <div className="flex flex-wrap justify-center ">
-        {categories.map((category, index) => {
-        const productImage = products.find(product => product.category === category);
-        return (
-          <div key={index} onClick={() => navigate('catalogue', category)}  className="cursor-pointer w-64 mx-4 my-4">
-            <ProductCategory
-              image={productImage.image}
-              category={category}
-            />
-        </div>
-        );
-        })}
+      <div className="flex flex-wrap justify-center">
+        {categories.length === 0 ? (
+          <p>No hay categorías disponibles</p>
+        ) : (
+          categories.map((category, index) => {
+            const productImage = products.find(product => product.category === category.category);
+            return (
+              <div key={index} onClick={() => navigate('catalogue', category.category)} className="cursor-pointer w-64 mx-4 my-4">
+                <ProductCategory
+                  image={category.image}
+                  category={category.category}
+                />
+              </div>
+            );
+          })
+        )}
+      </div>
     </div>
-    </div>
-);
+  );
 }
 
-ProductHomeList.propTypes = {
+ProducHomeList.propTypes = {
   products: PropTypes.array.isRequired,
+  categories: PropTypes.array.isRequired
 };
 
-export default ProductHomeList;
+export default ProducHomeList;
