dc6516e916e3a5fa24308fe1f19af479d17f234c
DANdelion <pen22217@uvg.edu.gt>
2024-05-22 23:15:28 -0600

    Add dynamic load of products

--- backend/src/api/routes/user.routes.js
+++ backend/src/api/routes/user.routes.js
@@ -8,161 +8,184 @@ const resend = new Resend("re_W7fQeeRt_Fx4JciPu3LhhBRU843mLEPmR");
 // Ruta unificada para obtener productos con cualquier combinación de filtros
 router.get('/products', productController.getAllProducts);
 
-const emailTemplate = `
-<!DOCTYPE html>
-<html>
-<head>
-<title>Confirmación de Cotización</title>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<style>
-* {
-  box-sizing: border-box;
-}
-
-body {
-  font-family: Arial, Helvetica, sans-serif;
-}
-
-.header {
-  background-color: #f1f1f1;
-  padding-right: 5%;
-  width: 100%;
-  text-align: right;
-  align-items: center;
-  justify-content: space-between;
-  font-size: x-large;
-  display: flex;
-}
-
-.row {
-  display: -webkit-flex;
-  display: flex;
-  text-align: center;
-  justify-content: center;
-  align-items: center;
-  border: 2px solid #aaa;
-}
-
-.column {
-  padding: 0.2em;
-  border: #aaa;
-}
-
-.column.side {
-  -webkit-flex: 1;
-  -ms-flex: 1;
-  flex: 1;
-}
-
-.column.sides {
-  -webkit-flex: 2;
-  -ms-flex: 2;
-  flex: 2;
-}
-
-.column.middle {
-  -webkit-flex: 3;
-  -ms-flex: 3;
-  flex: 3;
-}
-
-.footer {
-  background-color: #d1d1d1;
-  padding: 10px;
-  justify-content: end;
-  display: flex;
-  padding-right: 2%;
-}
-
-@media (max-width: 600px) {
-  .row {
-    -webkit-flex-direction: row;
-    flex-direction: column;
+const name = "Cliente";
+const quotationDetails = [
+  {
+    product: "Suéter Negro",
+    description: "Suéter de algodón",
+    quantity: 10,
+    unitPrice: 80.00,
+    total: 800.00,
+    image: "https://drs.com.gt/wp-content/uploads/2023/09/mapf1-sueter-negro2.png"
+  },
+  {
+    product: "Suéter Blanco",
+    description: "Suéter de algodón",
+    quantity: 10,
+    unitPrice: 80.00,
+    total: 800.00,
+    image: "https://drs.com.gt/wp-content/uploads/2023/09/mapf1-sueter-negro2.png"
   }
-}
-</style>
-</head>
-<body>
-  <div class="header">
-    <img width="250em" src="../../assets/imgs/logo.svg" alt="Logo de MK">
-    <h2>Confirmación de Cotización</h2>
-  </div>
+];
+const total = quotationDetails.reduce((sum, item) => sum + item.total, 0);
 
-  <div class="row">
-    <div class="column side" style="background-color:#aaa;">
+const emailTemplate = `
+  <!DOCTYPE html>
+  <html>
+  <head>
+  <title>Confirmación de Cotización</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <style>
+  * {
+    box-sizing: border-box;
+  }
+  body {
+    font-family: Arial, Helvetica, sans-serif;
+  }
+  ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+  }
+  .header {
+    background-color: #f1f1f1;
+    padding-right: 5%;
+    width: 100%;
+    text-align: right;
+    align-items: center;
+    justify-content: space-between;
+    font-size: x-large;
+    display: flex;
+  }
+  .row {
+    display: -webkit-flex;
+    display: flex;
+    text-align: center;
+    justify-content: space-evenly;
+    align-items: center;
+    border: 2px solid #aaa;
+  }
+  .column {
+    padding: 0.2em;
+    border: #aaa;
+    text-align: left;
+  }
+  .column.side {
+    -webkit-flex: 1;
+    -ms-flex: 1;
+    flex: 1;
+    justify-content: space-evenly;
+  }
+  .column.sides {
+    -webkit-flex: 2;
+    -ms-flex: 2;
+    flex: 2;
+    justify-content: space-evenly;
+  }
+  .column.middle {
+    -webkit-flex: 3;
+    -ms-flex: 3;
+    flex: 3;
+    justify-content: space-evenly;
+  }
+  .footer {
+    background-color: #d1d1d1;
+    padding: 10px;
+    justify-content: end;
+    display: flex;
+    padding-right: 10%;
+  }
+  img {
+    width: 100%;
+    max-width: 150px;
+    height: auto;
+  }
+  </style>
+  </head>
+  <body>
+    <p>Aquí está la confirmación de su cotización con los productos que ordenó.</p>
+    <div class="header">
+      <img heigth="100%" src="https://i.postimg.cc/DZZv68Bs/logo.png" alt="Logo de MK">
+      <h2>Confirmación de Cotización</h2>
     </div>
-    <div class="column sides" style="background-color:#aaa;">
-    </div>
-  </div>
 
-  <div class="row" style="background-color: #f1f1f1; text-align: left;">
-    <div class="column side" style="border-right: 2px solid #aaa;">
-      <ul>
-        <li>NIT:</li>
-        <li>Empresa:</li>
-        <li>Contacto:</li>
-        <li>Teléfono:</li>
-        <li>Dirección:</li>
-        <li>Correo:</li>
-      </ul>
+    <div class="row">
+      <div class="column side">
+      </div>
+      <div class="column sides">
+      </div>
     </div>
-    <div class="column sides">
-      <ul>
-        <li>De: Marielos Monterroso</li>
-        <li>Puesto: Ejecutiva de Proyectos</li>
-        <li>Correo: ventas@mkpublicidadgt.com</li>
-        <li>Cel: (502) 5524-5975</li>
-        <li>Oficina: (502) 2293-2986</li>
-      </ul>
-    </div>
-  </div>
 
-  <div class="row">
-    <div class="column side" style="background-color:#aaa;">
-      <h3>Producto</h3>
+    <div class="row" style="background-color: #f1f1f1;">
+      <div class="column side" style="border-right: 2px solid #aaa; text-align: left">
+        <ul>
+          <li>NIT: 123456789</li>
+          <li>Empresa: Y tal</li>
+          <li>Contacto: Jorge Pérez</li>
+          <li>Teléfono: 123456789</li>
+          <li>Dirección: Parque Z1</li>
+          <li>Correo: ytal@gmail.com</li>
+        </ul>
+      </div>
+      <div class="column sides">
+        <ul>
+          <li>De: Marielos Monterroso</li>
+          <li>Puesto: Ejecutiva de Proyectos</li>
+          <li>Correo: ventas@mkpublicidadgt.com</li>
+          <li>Cel: (502) 5524-5975</li>
+          <li>Oficina: (502) 2293-2986</li>
+        </ul>
+      </div>
     </div>
-    <div class="column sides" style="background-color:#aaa;">
-      <h3>Descripción</h3>
-    </div>
-    <div class="column middle" style="background-color:#aaa;">
-      <h3>Cantidad</h3>
-    </div>
-    <div class="column sides" style="background-color:#aaa;">
-      <h3>Precio unitario</h3>
-    </div>
-    <div class="column side" style="background-color:#aaa;">
-      <h3>Total</h3>
-    </div>
-  </div>
 
-  <div class="row" style="background-color: #f1f1f1;">
-    <div class="column side">
-      <img width="100%" src="../../assets/imgs/sueter_prueba.png" alt="Imagen del producto">
-      <p>Suéter Negro</p>
+    <div class="row">
+      <div class="column side">
+        <h3>Producto</h3>
+      </div>
+      <div class="column sides">
+        <h3>Descripción</h3>
+      </div>
+      <div class="column middle">
+        <h3>Cantidad</h3>
+      </div>
+      <div class="column sides">
+        <h3>Precio unitario</h3>
+      </div>
+      <div class="column side">
+        <h3>Total</h3>
+      </div>
     </div>
-    <div class="column sides">
-      <p>Suéter de algodón</p>
-    </div>
-    <div class="column middle">
-      <p>10</p>
-    </div>
-    <div class="column sides">
-      <p>Q80.00</p>
-    </div>
-    <div class="column side">
-      Q800.00
-    </div>
-  </div>
 
-  <div class="footer">
-    <h4 style="margin-right: 5em;">TOTAL</h4>
-    <h4>Q800.00</h4>
-  </div>
+    ${quotationDetails.map(item => `
+    <div class="row" style="background-color: #f1f1f1;">
+      <div class="column side">
+        <img heigth="50%" src=${item.image} alt="Imagen del producto">
+        <p>${item.product}</p>
+      </div>
+      <div class="column sides">
+        <p>${item.description}</p>
+      </div>
+      <div class="column middle">
+        <p>${item.quantity}</p>
+      </div>
+      <div class="column sides">
+        <p>Q${item.unitPrice.toFixed(2)}</p>
+      </div>
+      <div class="column side">
+        Q${item.total.toFixed(2)}
+      </div>
+    </div>
+    `).join('')}
 
-</body>
-</html>
-`
+    <div class="footer">
+      <h4 style="margin-right: 5em;">TOTAL</h4>
+      <h4>Q${total.toFixed(2)}</h4>
+    </div>
+
+  </body>
+  </html>
+`;
 
 // Ruta para enviar correos de confirmación al usuario. De momento solo puede enviar correos a mi dirección
 router.get("/send-email", async (req, res) => {
