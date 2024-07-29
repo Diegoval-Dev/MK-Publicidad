38eea95a0e8ed09a9f127a61d15457ddb15111da
Silviaillescas <silviaillesca402@gmail.com>
2024-07-28 20:15:59 -0600

    Add Banner component with carousel functionality

--- client-frontend/src/components/Banner.jsx
+++ client-frontend/src/components/Banner.jsx
@@ -1,13 +1,23 @@
+import React from 'react';
 import logo from '@assets/imgs/mk_logo.png';
-
+import '../styles/Banner.css'; 
 function Banner() {
   return (
-    <div className="relative w-full h-52 bg-cover bg-center">
-      <div className="absolute top-0 left-0 w-full h-8 bg-lime-700"></div>
-      <div className="w-full h-full bg-opacity-50 flex flex-col items-center justify-center">
-        <img src={logo} alt="Logo de MK" className="w-40 mb-4" />
+    <header className="header">
+      <div className="top-bar">
+        <div className="carousel">
+          <div className="carousel-content">
+            <span>Ponemos tu marca en todo...</span>
+          </div>
+        </div>
       </div>
-    </div>
+      <div className="relative w-full h-52 bg-cover bg-center">
+        <div className="absolute top-0 left-0 w-full h-8 bg-lime-700"></div>
+        <div className="w-full h-full bg-opacity-50 flex flex-col items-center justify-center">
+          <img src={logo} alt="Logo de MK" className="w-40 mb-4" />
+        </div>
+      </div>
+    </header>
   );
 }
 
