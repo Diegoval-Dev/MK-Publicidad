24c2805389dc318242e5d5f73c21cc23ae55f293
Daniel Penedo <pen22217@uvg.edu.gt>
2024-08-11 19:48:43 -0600

    Add suscription input

--- client-frontend/src/views/Contact.jsx
+++ client-frontend/src/views/Contact.jsx
@@ -37,7 +37,7 @@ function Contact() {
                 />
             </div>
 
-            <div className="container mx-auto border border-black-300 p-4 mt-5 shadow-md rounded-lg">
+            <div className="container w-[80%] mx-auto border border-black-300 p-4 mt-5 shadow-md rounded-lg">
                 <h1 className='centered-title'>Contáctanos</h1>
                 <hr className='mb-6'/>
                 <form onSubmit={handleSubmit} className='form'>
@@ -79,6 +79,25 @@ function Contact() {
                         Enviar
                     </button>
                 </form>
+
+                <div className='container border border-black-300 p-3 mt-5 shadow-md rounded-lg text-[white] mt-8 w-[100%] p-6 justify-between items-center text-left bg-[#388E3C] flex'>
+                    <div>
+                        <h1 className='boardTitle'>Boletín</h1>
+                        <p>Recibe nuestras novedades, promociones y recomendaciones</p>
+                    </div>
+                    <div className='flex items-center justify-center'>
+                        <input 
+                            className='input'
+                            type="text"
+                            name='emailBoard'
+                            placeholder='Correo electrónico'
+                        />
+                        <button
+                            type='submit'
+                            className={`font-bold bg-${hovered ? 'color-prices' : 'lime'}-500 text-${hovered ? 'componentes' : 'color-text'} rounded hover:bg-color-prices hover:text-color-componentes transition-colors`}
+                            >¡Estoy dentro!</button>
+                    </div>
+                </div>
             </div>
         </div>
     );
