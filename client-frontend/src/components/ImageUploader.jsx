c0c6edde082db4d34f016d442691e7c044643a4b
Silviaillescas <silviaillesca402@gmail.com>
2024-08-16 10:35:19 -0600

    Apply consistent button styles to ImageUploader component

--- client-frontend/src/components/ImageUploader.jsx
+++ client-frontend/src/components/ImageUploader.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { CustomButton } from '../styles/styled'; // Asegúrate de que la ruta es correcta
 
 const ImageUploader = ({ images, setImages }) => {
   const handleImageChange = (e) => {
@@ -22,13 +23,12 @@ const ImageUploader = ({ images, setImages }) => {
   return (
     <div>
       <input type="file" id="file-upload" className="hidden" onChange={handleImageChange} accept="image/*" />
-      <button
+      <CustomButton
         type="button"
-        className="text-sm bg-green-500 text-white py-2 px-4 rounded-md shadow-md hover:bg-green-600 border border-green-600 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 transition duration-150"
         onClick={() => document.getElementById('file-upload').click()}
       >
         Cargar Imagen
-      </button>
+      </CustomButton>
 
       <div className="mt-2 space-y-2">
         {images.map((image, index) => (
