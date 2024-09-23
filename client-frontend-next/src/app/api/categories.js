0c3824d8d679b3a8acf9b547b80770c87458d131
Diego <diegopablo012@gmail.com>
2024-09-23 16:00:05 -0600

    feat: module with the api endpoints

---
+++ client-frontend-next/src/app/api/categories.js
@@ -0,0 +1,38 @@
+
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
+/**
+ * Obtiene todas las categorías desde el backend.
+ * @returns {Promise<Array>} Lista de categorías.
+ * @throws {Error} Si la solicitud falla.
+ */
+export async function fetchCategories() {
+  try {
+    const response = await fetch(`${API_URL}/categories`);
+    if (!response.ok) {
+      throw new Error('Error al obtener las categorías');
+    }
+    return await response.json();
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
+
+/**
+ * Obtiene todas las categorías desde el backend.
+ * @returns {Promise<Array>} Lista de categorías.
+ * @throws {Error} Si la solicitud falla.
+ */
+export async function searchCategories(keyword) {
+  try {
+    const response = await fetch(`${API_URL}/search/categories?keyword=${encodeURIComponent(keyword)}`);
+    if (!response.ok) {
+      throw new Error('Error al buscar categorías');
+    }
+    return await response.json();
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
