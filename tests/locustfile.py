4eb214090ada12de0dbba8852f34dc199a927c81
Silviaillescas <silviaillesca402@gmail.com>
2024-09-26 18:30:53 -0600

    Add Locust configuration for API load testing

---
+++ tests/locustfile.py
@@ -0,0 +1,27 @@
+from locust import HttpUser, task, between
+
+class AdminUser(HttpUser):
+    wait_time = between(1, 3)  # Tiempo de espera entre las solicitudes, en segundos
+
+    # Prueba para el endpoint POST /api/admin/register
+    @task(1)
+    def register_user(self):
+        payload = {
+            "user_email": "newuser@example.com",
+            "user_password": "Password123!",
+            "user_role": "admin",
+            "user_name": "John Doe",
+            "position": "Manager",
+            "user_phone": "1234567890",
+            "user_officePhone": "0987654321"
+        }
+        self.client.post("/api/admin/register", json=payload)
+
+    # Prueba para el endpoint POST /api/admin/login
+    @task(2)  # Mayor peso porque se espera más actividad en login
+    def login_user(self):
+        payload = {
+            "user_email": "newuser@example.com",
+            "user_password": "Password123!"
+        }
+        self.client.post("/api/admin/login", json=payload)
