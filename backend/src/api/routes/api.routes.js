253ca89b170149a82913cfbd893322a7f4c1afc2
michellemej22596 <mej22596@uvg.edu.gt>
2024-10-20 00:15:25 -0600

    Feat: New route added

--- backend/src/api/routes/api.routes.js
+++ backend/src/api/routes/api.routes.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { createQuoteController } from '../controllers/quoteController.js'; // Asegúrate de que este controlador esté bien importado
+import { createQuoteController } from '../controllers/quoteController.js'; 
+import { updateQuoteController } from '../controllers/updateQuoteController.js'; 
 import authMiddleware from '../middlewares/authMiddleware.js'; // Middleware de autenticación
 import {uploadMiddleware} from '../middlewares/uploadMiddleware.js'; // Middleware de subida de archivos
 const router = express.Router();
 
 
-router.post('/quote', uploadMiddleware, createQuoteController);
+router.post('/quote', createQuoteController);
+quoteRouter.put('/quote/status', authMiddleware, updateQuoteController);
 
 export default router;
