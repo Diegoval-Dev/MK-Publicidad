6caaf8ae26b227fcf54a526d72cd6b05c2868ea6
Silviaillescas <silviaillesca402@gmail.com>
2024-11-15 11:49:45 -0600

    authcontext that you can only log in if you are already registered.

--- admin-frontend-next/src/context/AuthContext.js
+++ admin-frontend-next/src/context/AuthContext.js
@@ -1,7 +1,7 @@
 'use client';
 
 import { createContext, useState, useEffect } from 'react';
-import { useRouter, usePathname } from 'next/navigation';
+import { useRouter } from 'next/navigation';
 import jwtDecode from 'jsonwebtoken/decode';
 
 const AuthContext = createContext();
@@ -10,31 +10,17 @@ export function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
   const router = useRouter();
-  const pathname = usePathname(); // Hook para obtener la ruta actual
 
   useEffect(() => {
     const token = localStorage.getItem('token');
-
-    // Rutas que no necesitan autenticación
-    const publicRoutes = ['/login', '/register'];
-
     if (token) {
       const decoded = jwtDecode(token);
       setUser(decoded);
-
-      // Si el usuario está autenticado, redirigirlo fuera de `/login` y `/register`
-      if (publicRoutes.includes(pathname)) {
-        router.push('/dashboard'); // Cambia `/dashboard` por la ruta principal a donde redirigir al usuario autenticado
-      }
     } else {
-      // Si no hay token y está en una ruta protegida (no pública), redirigir al login
-      if (!publicRoutes.includes(pathname)) {
-        router.push('/login');
-      }
+      router.push('/login');
     }
-
     setLoading(false);
-  }, [router, pathname]);
+  }, [router]);
 
   if (loading) {
     return <div>Cargando...</div>;
@@ -47,4 +33,4 @@ export function AuthProvider({ children }) {
   );
 }
 
-export default AuthContext;
+export default AuthContext; 
